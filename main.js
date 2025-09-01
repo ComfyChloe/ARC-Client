826ec016271729c961452e57638bf13dbf87df67
ComfyChloe <minerspark@hotmail.com>
2025-09-01 06:41:45 +0100

    Revert "More stable state sync between server and osc-client"
    This reverts commit c3a7b4cc4287f5c1a70d7c3946f337733f5012a8.
    
    	modified:   docs/AVATAR_STATE_SYNC.md
    	modified:   osc-client/main.js
    	modified:   osc-client/renderer/index.html
    	modified:   src/websocket/client_ws_manager.js

--- main.js
+++ main.js
@@ -251,17 +251,18 @@ function initOscServer() {
     if (oscMessageCounter === 1 || oscMessageCounter % 100 === 0) {
       debug.oscMessageReceived(data.address, data.value, data.type);
     }
-    // Always forward to WebSocket if connected (outgoing data should flow to enable remote control)
+    // Only forward to WebSocket if both OSC and WebSocket forwarding are enabled
     const wsConnected = wsManager && wsManager.isConnected;
-    let shouldForward = wsConnected;
-    // For additional connections, still respect their individual WebSocket forwarding setting
-    if (data.connectionId && wsConnected) {
+    let wsForwardingEnabled = serverConfig.appSettings?.enableWebSocketForwarding || false;
+    // If this message comes from an additional connection, check its individual forwarding setting
+    if (data.connectionId) {
       const connection = serverConfig.additionalOscConnections?.find(conn => conn.id === data.connectionId);
       if (connection && connection.type === 'incoming') {
-        shouldForward = connection.enableWebSocketForwarding || false;
+        // For incoming additional connections, use their individual WebSocket forwarding setting
+        wsForwardingEnabled = connection.enableWebSocketForwarding || false;
       }
-    } 
-    if (shouldForward) {
+    }
+    if (wsConnected && wsForwardingEnabled) {
       // Check if the parameter is blacklisted before forwarding
       if (!parameterBlacklist.isBlacklisted(data.address)) {
         try {
@@ -284,9 +285,12 @@ function initOscServer() {
         debug.logWebSocketForwarding(`Parameter blacklisted - not forwarding: ${data.address}`);
       }
     } else {
-      // Only log for additional connections that have forwarding disabled
-      if (wsConnected && data.connectionId) {
-        debug.logWebSocketForwarding(`Additional connection WebSocket forwarding disabled - not forwarding: ${data.address}`);
+      if (wsConnected && !wsForwardingEnabled) {
+        if (data.connectionId) {
+          debug.logWebSocketForwarding(`Additional connection WebSocket forwarding disabled - not forwarding: ${data.address}`);
+        } else {
+          // debug.logWebSocketForwarding(`WebSocket forwarding disabled - not forwarding: ${data.address}`);
+        }
       }
     }
     if (!data.connectionId && oscService) {
